353a96d600aee81baf70d40e507a86eedece8120
alstdu <lexi.dugo@gmail.com>
2024-07-16 10:47:30 -0700

    Import native components and axios

---
+++ components/SpellTable.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import axios from 'axios';
